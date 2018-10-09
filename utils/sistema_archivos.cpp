d907965742a40f8a2bcd485a8369c92daec6cc0f
Miguel Lorenzo Lorenzo <MiguelAngel.Lorenzo@Honeywell.com>
2018-10-09 17:13:25 -0500

    intento de compatibilizar windows con linux. boost::filesystem usado en windows debido a incompatibilidad con std::fs

--- utils/sistema_archivos.cpp
+++ utils/sistema_archivos.cpp
@@ -2,9 +2,14 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#ifdef _WIN32
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#else
 #include <experimental/filesystem>
-
 namespace fs = std::experimental::filesystem;
+#endif
+
 using namespace std;
  
 vector<string> get_file_names(std::string dir_name)
@@ -12,7 +17,11 @@ vector<string> get_file_names(std::string dir_name)
     vector<string> files;
     std::cout << "iterando sobre " << dir_name << std::endl;
     for(auto& p: fs::directory_iterator(dir_name)) {
+        #ifdef _WIN32
+        string s = p.path().string();
+        #else
         string s = p.path();
+        #endif
         std::cout << s << '\n';
         files.push_back(s);
     }
